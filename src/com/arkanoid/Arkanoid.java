07289e26110ce0b6b49baf4ca2b4f13e8119e1a2
kew1985no@gmail.com <kew1985no@gmail.com@04680a5a-a0fb-00f2-2593-341559c3ab33>
2011-02-18 15:26:03 +0000

    La til en kommentar. For å sjekke at commit fungerer.

---
+++ src/com/arkanoid/Arkanoid.java
@@ -0,0 +1,14 @@
+package com.arkanoid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Arkanoid extends Activity {
+    /** Called when the activity is first created. */
+	// en kommentar lol!
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
