eb0fb7f16a95bfcf8b82c035110f994124cfda46
dahl_lia@hotmail.com <dahl_lia@hotmail.com@04680a5a-a0fb-00f2-2593-341559c3ab33>
2011-04-04 17:18:43 +0000

    lagt til Wall objekt

---
+++ src/com/arkanoid/Wall.java
@@ -0,0 +1,13 @@
+package com.arkanoid;
+
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+
+public class Wall extends Sprite{
+
+	public Wall(float pX, float pY, TextureRegion pTextureRegion) {
+		super(pX, pY, pTextureRegion);
+		// TODO Auto-generated constructor stub
+	}
+
+}
