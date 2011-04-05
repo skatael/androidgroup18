1fc614ac40c69dfc6a32fe412ef9ec7594343b95
dahl_lia@hotmail.com <dahl_lia@hotmail.com@04680a5a-a0fb-00f2-2593-341559c3ab33>
2011-04-05 10:05:03 +0000

    litt mer kode i Block

--- src/com/arkanoid/Block.java
+++ src/com/arkanoid/Block.java
@@ -5,12 +5,25 @@ import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
 public class Block extends Sprite{
 	
+	private int lives;
+	private int worth;
+	private String powerup;
+	
+	//temp constructor
 	public Block(float pX, float pY, TextureRegion pTextureRegion) {
 		super(pX, pY, pTextureRegion);
 		
 		// TODO Auto-generated constructor stub
 	}
 	
+	//final constructor
+//	public Block(float pX, float pY, TextureRegion pTextureRegion, int lives, int worth, String powerup) {
+//		super(pX, pY, pTextureRegion);
+//		this.lives = lives;
+//		this.worth = worth;
+//		this.powerup = powerup;
+//		// TODO Auto-generated constructor stub
+//	}
 	
 	public float GetX(){
 		return this.getX();
@@ -19,7 +32,12 @@ public class Block extends Sprite{
 		return this.getY();
 	}
 	public void gotHit(){
-		
+		lives =lives -1;
+		if(lives <= 0){
+			//display explosion el no sånn
+			
+			//create powerup?
+		}
 	}
 
 }
