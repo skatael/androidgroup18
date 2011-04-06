ee35f0fdd04e7855812d8e5723340e5f321b25bf
kew1985no@gmail.com <kew1985no@gmail.com@04680a5a-a0fb-00f2-2593-341559c3ab33>
2011-04-06 10:55:52 +0000

    Ballcontroller class

---
+++ trunk/src/com/arkanoid/BlockController.java
@@ -0,0 +1,64 @@
+package com.arkanoid;
+
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+
+public class BlockController {
+	
+	private Arkanoid ark;
+	//Block Textures
+    private Texture blTexture;
+    private TextureRegion mBlockTextureRegion;
+    
+    //
+    private final Block[] blocks;
+	
+	public BlockController(Arkanoid ark){
+		this.ark = ark;
+		blTexture = new Texture(128, 32, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        mBlockTextureRegion = TextureRegionFactory.createFromAsset(this.blTexture, ark, "gfx/block.png", 0, 0);
+        ark.getEngine().getTextureManager().loadTexture(this.blTexture);
+        
+        blocks = makeBlocks(); 
+  
+		
+	}
+	
+	private Block[] makeBlocks(){
+		
+		
+		int level = 2;
+        int[] blockArray = BlockLayout.MakeBlocks(level);
+        int j = 0;
+        
+        Block[] blocks = new Block[6];
+        
+        int xPos =40, yPos = 50;
+
+        for(int i = 0;i < blockArray.length; i++){
+        	if(blockArray[i] == 1){
+        		blocks[j] = new Block(xPos, yPos, this.mBlockTextureRegion);
+        		xPos+=128;
+        		j++;
+        	}else if(blockArray[i] == 2){
+        		blocks[j] = new Block(xPos, yPos, this.mBlockTextureRegion);
+        		xPos+=128;
+        		j++;
+        	}else if(blockArray[i] == 0){
+        		yPos+=32;
+        		xPos = 40;
+        	}
+        }
+		
+		
+		return blocks;
+	}
+	
+	public Block[] getBlocks(){
+		return blocks;
+	}
+	
+
+}
