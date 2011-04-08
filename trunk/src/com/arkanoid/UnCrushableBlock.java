ee36bd10a49fa402d9f01685429cdc54cc8b6426
kew1985no@gmail.com <kew1985no@gmail.com@04680a5a-a0fb-00f2-2593-341559c3ab33>
2011-04-08 09:52:16 +0000

    javadoc

--- trunk/src/com/arkanoid/UnCrushableBlock.java
+++ trunk/src/com/arkanoid/UnCrushableBlock.java
@@ -3,28 +3,47 @@ package com.arkanoid;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
 import com.arkanoid.Block;
-
+/**
+ * class used for creating undestroyable blocks
+ * @author kenneth
+ *
+ */
 public class UnCrushableBlock extends Block{
 	private int lives;
 	private int worth;
 	private String powerup;
 	
+	/**
+	 * constructor
+	 * @param pX x-coordinate of the block
+	 * @param pY y-coordinate of the block
+	 * @param pTextureRegion textureregion of the block
+	 * @param ark pointer to the main class
+	 */
 	public UnCrushableBlock(float pX, float pY, TextureRegion pTextureRegion, Arkanoid ark) {
 		super(pX, pY, pTextureRegion, ark);
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * @return x-coordinate
+	 */
 	public float GetX(){
 		return this.getX();
 	}
+	/**
+	 * @return y-coordinate
+	 */
 	public float GetY(){
 		return this.getY();
 	}
+	/**
+	 * method called when the block is hit
+	 */
 	public void gotHit(){
-		if(lives <= 0){
-			//display explosion el no s�nn
+		
 			
-			//create powerup?
-		}
 	}
-
 }
+
+
+
